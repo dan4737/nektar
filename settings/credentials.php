83c6ce73435bbf908abf69873a4b8019b9ba1688
dan4737 <stephenofori444@gmail.com>
2022-11-19 23:48:29 +0000

    nnn

--- settings/credentials.php
+++ settings/credentials.php
@@ -3,9 +3,20 @@
 // set up constants for database connection
 // database, user, password, server
 
-define('SERVER', 'localhost');
-define('USER', 'root');
-define('DATABASE', 'nectar');
-define('PASSWORD', '');
+// define('SERVER', 'localhost');
+// define('USER', 'root');
+// define('DATABASE', 'nectar');
+// define('PASSWORD', '');
+
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USER', 'bb9cfd5c84bff9');
+define('DATABASE', 'e35cc31f');
+define('PASSWORD', 'heroku_cdeaa0234ec03b4');
+
+
+
+
+
+mysql://bb9cfd5c84bff9:e35cc31f@us-cdbr-east-06.cleardb.net/heroku_cdeaa0234ec03b4?reconnect=true
 
 ?>
